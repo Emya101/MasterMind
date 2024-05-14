57223fa22f348139b86d9022a0f07e8894638e20
Emya101 <160042834+Emya101@users.noreply.github.com>
2024-05-13 23:39:21 -0500

    Add files via upload

---
+++ Colour.java
@@ -0,0 +1,8 @@
+//Emhenya Supreme 3132969
+/**
+ * The Colour enum represents different colors that can be used in the game.
+ */
+
+public enum Colour {
+        RED,YELLOW,GREEN,BLUE,BLACK,WHITE;
+}
